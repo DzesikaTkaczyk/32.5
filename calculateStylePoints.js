179af0cf9ff1e6ebaa47d40261f80ca19a0bba55
DzesikaTkaczyk <50298851+DzesikaTkaczyk@users.noreply.github.com>
2019-11-29 09:07:51 +0100

    Update calculateStylePoints.js

--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -2,8 +2,8 @@ const calculateStylePoints = (styleNotes) => {
 	let sumNotes = 0;
 	for( i = 0; i < 5; i++ ) {
 		sumNotes += styleNotes[i]
-	}
-	return sumNotes - Math.min(...styleNotes) - Math.max(...styleNotes)
+	};
+	return sumNotes - Math.min(...styleNotes) - Math.max(...styleNotes);
 };
 
 module.exports = calculateStylePoints;
