924f44d1dd335b500cc97a157cc56fccf13cbe1d
DzesikaTkaczyk <50298851+DzesikaTkaczyk@users.noreply.github.com>
2019-11-29 09:08:15 +0100

    Update calculateDistancePoints.js

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -2,13 +2,13 @@ const calculateDistancePoints = (distance, hillSize, kPoint) => {
 	let distancePoints = 0;
 	if( hillSize === 'mamucia' ){
 		distancePoints = 120 + ((distance - kPoint) * 1.2)
-	}
+	};
 	else if (hillSize === 'big') {
 		distancePoints = 60 + ((distance - kPoint) * 1.8)
-	}
+	};
 	else if (hillSize === 'normal') {
 		distancePoints = 60 + ((distance - kPoint) * 2)
-	}
-	return distancePoints
+	};
+	return distancePoints;
 };
 module.exports = calculateDistancePoints;
